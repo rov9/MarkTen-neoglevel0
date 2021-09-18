e6a2783501cad5a37468aa7209767176fcf39157
rov9 <duttasaurabh92@gmail.com>
2021-09-18 13:41:17 +0530

    html css js update

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ checkButton.addEventListener("click",function validateBillAndCashAmount(){
 
 function calculateChange(amountToBeReturned)
 {
-for(let i=0;i<=availableNotes.length();i++){
+for(let i=0;i< availableNotes.length;i++){
     const numberOfNotes= Math.trunc(amountToBeReturned/availableNotes[i]);
 
     noOfNotes[i].innerText=numberOfNotes;
