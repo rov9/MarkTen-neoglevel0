74588512c51643600f86aea6085f89cc706e8e69
rov9 <duttasaurabh92@gmail.com>
2021-09-21 15:02:23 +0530

    html,js update

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click",function validateBillAndCashAmount(){
+checkButton.addEventListener("click",function validateBillAndCashAmount()
+{
     hideMessage();
+
     if(billAmount.value>0)
     {
         if (cashGiven.value >= billAmount.value) {
 
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+            var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
     }
     else{
-        showMessage("Do you wanna wash plates?");
+        showMessage("Please provide bill amount else will you wash plates?");
     }
   } else {
     showMessage("Invalid Amount!");
@@ -25,9 +27,9 @@ checkButton.addEventListener("click",function validateBillAndCashAmount(){
 
 function calculateChange(amountToBeReturned)
 {
-for(let i=0;i< availableNotes.length;i++){
-    const numberOfNotes= Math.trunc(amountToBeReturned/availableNotes[i]);
-
+for(let i=0;i<availableNotes.length;i++){
+    var numberOfNotes= Math.trunc(amountToBeReturned/availableNotes[i]);
+    var amountToBeReturned = amountToBeReturned % availableNotes[i];
     noOfNotes[i].innerText=numberOfNotes;
 }
 }
